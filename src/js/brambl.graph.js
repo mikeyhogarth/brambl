e6a417d78c33d9dd24d090906b75a21d2881f97b
Mike Hogarth <mikehogarth20@hotmail.com>
2016-08-19 17:49:39 +0100

    Renderer

--- src/js/brambl.graph.js
+++ src/js/brambl.graph.js
@@ -1,4 +1,5 @@
-var d3    = require('d3-force');
+var d3        = require('d3-force');
+var Renderer  = require('./brambl.renderer'); 
 
 class Graph {
 
@@ -12,17 +13,13 @@ class Graph {
     this.edges      = data.edges || [];
     this.canvas     = document.querySelector(selector);
     this.simulation = null; 
+    this.renderer   = new Renderer(this);
   }
 
-  // Lots of horrible side effects in here at the moment - this is just
-  // because the code here is largely a copy/paste job from a d3 example
-  // and this whole thing is basically just in "bootstrapping mode" at the 
-  // moment while I get the env set up the way I want it. 
-  //
-  // Don't judge me!!
-  //
+  /**
+   * start the simulation
+  */
   start() {
-    var context    = this.canvas.getContext("2d");
     this.simulation = d3.forceSimulation()
         .force("link", d3.forceLink().id(d => d.id))
         .force("charge", d3.forceManyBody())
@@ -30,36 +27,20 @@ class Graph {
 
     this.simulation
         .nodes(this.nodes)
-        .on("tick", redraw.bind(this));
 
     this.simulation.force("link")
         .links(this.edges)
         .distance(d =>  100);
 
-    function redraw() {
-      context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-      context.beginPath();
-      this.edges.forEach(drawLink);
-      context.stroke();
-
-      context.beginPath();
-      this.nodes.forEach(drawNode);
-      context.fill();
-    }
-
-    function drawLink(d) {
-      context.moveTo(d.source.x, d.source.y);
-      context.lineTo(d.target.x, d.target.y);
-    }
-
-    function drawNode(d) {
-      context.moveTo(d.x + 3, d.y);
-      context.arc(d.x, d.y, 20, 0, 2 * Math.PI);
-    }
-
+    this.simulation.on("tick", () => this.renderer.redraw());
   }
-  
+
+  /**
+   * stop the simulation
+   */
+  stop() {
+    this.simulation.stop();
+  }
 }
 
 module.exports = Graph; 
