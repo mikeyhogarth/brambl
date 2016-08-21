1b243dd0ac35563fa49acfbe328436b90bcb77a8
Mike Hogarth <mikehogarth20@hotmail.com>
2016-08-21 11:39:40 +0100

    Basic lables

--- src/js/renderers/brambl.node.renderer.js
+++ src/js/renderers/brambl.node.renderer.js
@@ -1,4 +1,8 @@
 module.exports = (d, ctx) => {
   ctx.moveTo(d.x + 3, d.y);
   ctx.arc(d.x, d.y, 20, 0, 2 * Math.PI);
+
+  // label
+  ctx.font = "20px Georgia";
+  ctx.fillText(d.id, d.x + 10, d.y + 40);
 };
